4fbfd9ff55aa7d4685592119b087c89913958124
0653Jerico <markuzkrei432@gmail.com>
2024-07-21 22:02:48 +0800

    Understand for loop
    Added some js codes learning

---
+++ FCC/gradeBook.js
@@ -0,0 +1,51 @@
+function getAverage(scores){
+    let sum = 0;
+
+    for (let i = 0; i < scores.length; i++){ //This will iterate through all the grade points
+        sum += scores[i]; //This will add all iterated grade points
+    }
+
+    let average = sum / scores.length;
+
+    return average;
+}
+
+// console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+// console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+function getGrade(score){
+    if (score === 100){
+        return "A++";
+    }else if (score >= 90){
+        return "A";
+    }else if (score >= 80){
+        return "B";
+    }else if (score >= 70){
+        return "C";
+    }else if (score >= 60){
+        return "D";
+    }else{
+        return "F";
+    }
+}
+
+// console.log(getGrade(96));
+// console.log(getGrade(82));
+// console.log(getGrade(56));
+
+function hasPassingGrade(score){
+    let check = getGrade(score);
+    if (check === "F"){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+// console.log(hasPassingGrade(100));
+// console.log(hasPassingGrade(53));
+// console.log(hasPassingGrade(87));
+
+function studentMsg(totalScores, studentScore){
+    
+}
